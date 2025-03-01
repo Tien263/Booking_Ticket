7e8aabf8944f8d45fb92341101eccfa2ecff9ebf
PhuongAnh-01 <anhttphe181589@fpt.edu.vn>
2025-03-01 07:02:29 +0700

    employee and customer can view blog, not edited time to correct form, interface

--- bookingg/src/java/controller/accesscontrol/CusListBlogController.java
+++ bookingg/src/java/controller/accesscontrol/CusListBlogController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.accesscontrol;
 
 import dal.BlogDao;
@@ -20,40 +19,43 @@ import model.Blog;
  * @author ADMIN
  */
 public class CusListBlogController extends HttpServlet {
-   
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet CusListBlogController</title>");  
+            out.println("<title>Servlet CusListBlogController</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet CusListBlogController at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet CusListBlogController at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
+    private BlogDao blogDao;
 
-    
+    @Override
+    public void init() throws ServletException {
+        blogDao = new BlogDao();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        BlogDao bdao = new BlogDao();
-        List<Blog> listB = bdao.list();
-        
-        request.setAttribute("listB", listB);
+            throws ServletException, IOException {
+        List<Blog> blogList = blogDao.getAllBlogs();
+
+        request.setAttribute("blogList", blogList);
         request.getRequestDispatcher("blog.jsp").forward(request, response);
 
-
-    } 
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
@@ -62,14 +64,13 @@ public class CusListBlogController extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
-   
     public static void main(String[] args) {
         // Tạo instance của BlogDao
         BlogDao bdao = new BlogDao();
-        
+
         // Lấy danh sách blog
         List<Blog> listB = bdao.list();
-        
+
         // Kiểm tra danh sách có dữ liệu không
         if (listB.isEmpty()) {
             System.out.println("Không có blog nào trong danh sách.");
