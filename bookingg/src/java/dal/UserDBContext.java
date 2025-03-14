28a79d59ca12d04310fa7c2cf80a811203793662
ducnmcoder <duc29081234@gmail.com>
2025-03-14 12:53:22 +0700

    thêm USERDBContext

--- bookingg/src/java/dal/UserDBContext.java
+++ bookingg/src/java/dal/UserDBContext.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.User;
+import model.User1;
 
 /**
  *
@@ -68,6 +69,47 @@ public class UserDBContext extends DBContext<User> {
         }
         return roles;
     }
+    
+      public List<User1> getUserTransactions() {
+        List<User1> userList = new ArrayList<>();
+        String sql = "SELECT c.c_id, c.c_fullname, c.c_email, o.oh_id, o.oh_date, o.oh_status, o.oh_totalAmount, " +
+                     "p.p1_date, p.p1_amount, pm.pm_name, t.t_id, t.t_status, bt.bt1_departureTime, " +
+                     "bt.bt1_arrivalTime, i.i_totalPrice " +
+                     "FROM dbo.Customer c " +
+                     "JOIN dbo.Users u ON c.c_username = u.u_username " +
+                     "JOIN dbo.Bill b ON c.c_id = b.b_id " +
+                     "JOIN dbo.Payment p ON b.p1_id = p.p1_id " +
+                     "JOIN dbo.PaymentMethods pm ON p.p1_id = pm.p1_id" +
+                     "JOIN dbo.OrderHistory o ON p.p1_id = o.p1_id " +
+                     "JOIN dbo.Tickets t ON o.oh_id = t.t_id " +
+                     "JOIN dbo.BusTrips bt ON t.bt1_id = bt.bt1_id " +
+                     "JOIN dbo.Invoice i ON p.i_id = i.i_id" +
+                     "ORDER BY o.oh_date DESC";
+               
+        try (
+                PreparedStatement ps = connection.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                User1 user = new User1(
+                        rs.getString("c_fullname"),
+                        rs.getString("c_email"),
+                        rs.getString("c_CCCD"),
+                        rs.getString("oh_date"),
+                        rs.getString("p1_date"),
+                        rs.getString("pm_name"),
+                        rs.getInt("t_id"),
+                        rs.getString("bt1_departureTime"),
+                        rs.getString("bt1_arrivalTime"),
+                        rs.getString("t_status"),
+                        rs.getDouble("oh_totalAmount")
+                );
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
+    }
+
 
     // Lấy thông tin User theo username
     public User getUserByUsername(String username) {
